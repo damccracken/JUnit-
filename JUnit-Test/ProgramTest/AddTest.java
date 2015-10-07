7c8d1d22a5b1cfd01db884633c5f96b7776377a5
damccracken <damccracken@crimson.ua.edu>
2015-10-07 15:31:45 -0500

    First

---
+++ JUnit-Test/ProgramTest/AddTest.java
@@ -0,0 +1,17 @@
+package ProgramTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AddTest {
+
+	@Test
+	public void Addtest() {
+		MyJUnitClass junit = new MyJUnitClass();
+		int result = junit.add(100, 200);
+		assertEquals(300, result);
+		//WORK
+	}
+
+}
